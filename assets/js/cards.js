824c41b72a7119606fa44c95eb4bf8a6335adaec
Paul Pangilinan <2216511@slu.edu.ph>
2024-05-04 00:22:04 +0800

    Added html page for book, not much done yet

---
+++ assets/js/cards.js
@@ -0,0 +1,9 @@
+// For book page functions
+// TO DO:
+// Insert and change pictures and text
+// Add page number for navigation
+// Get data from JSON file
+
+let test= ["hello", "place", "for", "me!"]
+
+function insertText(){}
